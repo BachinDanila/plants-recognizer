568bc40192ae795c9bef0874bcf5e1e07950b492
BachinDanila <nikto985@gmail.com>
2018-09-20 21:41:30 +0300

    Add dynamically changing themes

--- source/Java/app/src/main/java/com/example/plantsrecognizer/MainActivity.java
+++ source/Java/app/src/main/java/com/example/plantsrecognizer/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.plantsrecognizer;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,31 +12,46 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int SETTINGS_ACTION = 1;
     public Button settings_button;
-    public static ConstraintLayout mConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        SharedPreferences pref = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        String themeName = pref.getString("theme", "AppTheme");
+        String galleryName = pref.getString("gallery", "A");
+
+        if (themeName.equals("Theme1")) {
+            setTheme(R.style.Theme1);
+        }
+
+        else if (themeName.equals("Theme2")) {
+            //Toast.makeText(this, "Theme has been reset to " + themeName, Toast.LENGTH_SHORT).show();
+            setTheme(R.style.Theme2);
+        }
+
+        else if (themeName.equals("AppTheme")){
+            setTheme(R.style.AppTheme);
+        }
+
+        Log.v("Gallery: ",galleryName);
+        Log.v("themeName",themeName);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mConstraintLayout = findViewById(R.id.constraintLayout);
-        settings_button = findViewById(R.id.settings_button);
 
+        settings_button = findViewById(R.id.settings_button);
 
         OnClickListener onClick_Handler = new OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.settings_button:
-                        Intent settings_intent = new Intent(MainActivity.this, SettingsActivity.class);
-                        startActivity(settings_intent);
+                        startActivityForResult(new Intent(MainActivity.this, ThemePreferenceActivity.class), SETTINGS_ACTION);
                         break;
-
-//                    case R.id.about_button:
-//                        Intent about_intent = new Intent(MainActivity.this, AboutActivity.class);
-//                        startActivity(about_intent);
-//                        break;
 //
 //                    case R.id.how_to_use_button:
 //                        Intent how_to_use_intent = new Intent(MainActivity.this, HowToUseActivity.class);
@@ -56,5 +71,21 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         settings_button.setOnClickListener(onClick_Handler);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SETTINGS_ACTION) {
+            if (resultCode == ThemePreferenceActivity.RESULT_CODE_THEME_UPDATED) {
+                finish();
+                startActivity(getIntent());
+                return;
+            }
+            if (resultCode == ThemePreferenceActivity.RESULT_CODE_GALLERY_UPDATED){
+                finish();
+                startActivity(getIntent());
+                return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
